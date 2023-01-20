950fd33a50ca71c5fd258d72be653975840f036a
Matias Aguirre <aguirremati09@gmail.com>
2023-01-20 01:42:38 -0300

    Project Finished

---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(
+  [
+    { text: 'Hello' },
+    { text: 'Welcome' },
+    { text: 'Greeting' },
+    { text: 'HEY!!' },
+    { text: 'HI!' },
+    { text: 'Welcome Back' },
+    { text: 'Nice to See you' },
+    { text: 'wassaaaa' },
+    { text: 'hola!!!' }
+  ]
+)
